e9ba7bdc35e56c08b305bdb7c1b6467616840b38
Kaleb Dickerson <kaleb.dickerson2001@dive3.engr.tamu.edu>
2021-07-21 10:22:46 -0700

    initial commit

---
+++ create_kdd_dataset3D.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(1,'/data3/kaleb.dickerson2001/Datasets/PubChem3D/db_api.py')
+from db_api import Reader
+
+reader = Reader('/data3/kaleb.dickerson2001/Datasets/PubChem3D/smile_coord.db')
+coordinates = reader.get_coords("smiles string here")
+
+
+sys.path.insert(1,'/home/ugrads/k/kaleb.dickerson2001/pcqm4m_dataset_with_coordinates/ogb')
+from ogb.lsc import PygPCQM4MDataset, PCQM4MEvaluator
+from ogb.utils import smiles2graph
+import torch
+from torch_geometric.data import DataLoader
+import numpy as np
+
+# for this to work, processes folder must be empty
+# uses modified ogb code
+dataset = PygPCQM4MDataset(
+    root="/data3/kaleb.dickerson2001/Datasets/KDD-pyg-dataset",
+    smiles2graph=smiles2graph,
+)
+
+dic = torch.load("/data3/kaleb.dickerson2001/Datasets/KDD-pyg-dataset/pcqm4m_kddcup2021/split_dict.pt")
+
+split_idx = dataset.get_idx_split()
+batch_size = 256
+train_loader = DataLoader(dataset[split_idx["train"]], batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(dataset[split_idx["test"]], batch_size=batch_size, shuffle=False)
+
+count = 0
+
+print(dic)
+print(count)
