d43330829a8dfc97890f7d9b4a49558069b901e8
Kaleb Dickerson <kaleb.dickerson2001@dive3.engr.tamu.edu>
2021-07-26 15:16:39 -0700

    new util functions

--- utils.py
+++ utils.py
@@ -3,8 +3,9 @@ import pubchempy
 from numpy import loadtxt
 from threading import Thread
 import numpy as np
-from database_api import Writer
 import os
+import pubchempy as pcp
+from tqdm import tqdm
 
 class ThreadWithReturn(Thread):
     def __init__(self, group=None, target=None, name=None,
@@ -18,29 +19,6 @@ class ThreadWithReturn(Thread):
     def join(self, *args):
         Thread.join(self, *args)
         return self._return
-
-
-def CID_from_SMILES(smile):
-    compounds = pubchempy.get_compounds(smile, namespace='smiles')
-    cid = compounds[0].cid
-    smiles_1 = compounds[0].canonical_smiles
-
-    mol = Chem.MolFromSmiles(smile)
-    smiles_canonical = Chem.MolToSmiles(mol, canonical=True)
-
-
-    mol_1 = Chem.MolFromSmiles(smiles_1)
-    smiles_1_canonical = Chem.MolToSmiles(mol_1, canonical=True)
-
-    if smiles_1_canonical == smiles_canonical:
-        # print('cid: ', cid)
-        # print(smiles_canonical)
-        return cid
-    else:
-        # print('Not found')
-        # print(smiles_1_canonical)
-        # print(smiles_canonical)
-        return -1
 
 def get_coordinates(file_path, error_log=None) -> str:
     """
@@ -63,35 +41,47 @@ def get_coordinates(file_path, error_log=None) -> str:
             error_log.write(f"error in get_coordinates at file: {file_path}\n")
         return -1
 
-def split_into(file_list, num_splits):
-    chunk = np.array_split(np.array(file_list),num_splits)
+def split_into(iterable, num_splits):
+    chunk = np.array_split(np.array(iterable),num_splits)
     for i in range(len(chunk)):
         chunk[i] = list(chunk[i])
     
     return chunk
 
-def combine_databases(target_db:str, path:str) -> None:
-    """
-    combines all databases from target directory with
-    the target database
-    """
-    files = []
-    for file in os.listdir(path):
-        if file.endswith(".db") and file != target_db:
-            files.append(file)
-    writer = Writer(target_db)
-    for db in files:
-        writer.combine_with(db)
+def smile2CID(smile):
+    compounds = pubchempy.get_compounds(smile, namespace='smiles')
+    cid = compounds[0].cid
+    return cid
 
-# def get_smiles(file_path) -> str:
+def CIDs2smiles(CID_list):
+    Lists = []
+
+    num_splits = len(CID_list) // 1000
+    if num_splits == 0:
+        num_splits = 1
+    # call the pubchem api in batches of 1000
+    CID_split = split_into(CID_list, num_splits)
+
+    for cids in tqdm(CID_split):
+        compounds = pcp.get_compounds(cids, as_dataframe=False)
+        to_append = np.empty(len(compounds), dtype=tuple)
+        for i in range(len(compounds)):
+            to_append[i] = (compounds[i].canonical_smiles, compounds[i].cid)
+        Lists.append(to_append)
+    
+    to_return = np.concatenate(Lists)
+    return to_return
+
+
+# def combine_databases(target_db:str, path:str) -> None:
 #     """
-#     input: .info file
-#     return: smiles string
+#     combines all databases from target directory with
+#     the target database
 #     """
-#     # simply returns smiles
-#     try:
-#         smile = loadtxt(file_path, dtype=str)[3]
-#         return smile
-#     except:
-#         error_log.write(f"error in get_smiles at file: {file_path}\n")
-#         return "NONE"
+#     files = []
+#     for file in os.listdir(path):
+#         if file.endswith(".db") and file != target_db:
+#             files.append(file)
+#     writer = Writer(target_db)
+#     for db in files:
+#         writer.combine_with(db)
